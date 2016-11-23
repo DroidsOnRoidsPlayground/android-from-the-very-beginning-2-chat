708ece40080813b88dab82bcea682edac414df95
paulina.szklarska <paulina.szklarska@droidsonroids.pl>
2016-11-23 14:56:21 +0100

    Initial commit

---
+++ app/src/main/java/pl/droidsonroids/czatexample/Message.java
@@ -0,0 +1,14 @@
+package pl.droidsonroids.czatexample;
+
+public class Message {
+
+    private String message;
+
+    public Message(final String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
